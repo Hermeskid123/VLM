a9f7f3ed41ce7c5d7e5eb5723bbf68415048d086
Hermeskid123 <preston.mann@mavs.uta.edu>
2022-09-16 14:21:35 -0500

    added test.py

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ def get_random_images():
     os.chdir(path)
     ran = random_with_0_digits()
     random_num = "0"+str(ran)
-    depth_name ="depth_3_0018190.png"
+    depth_name ="depth_1_0018190.png"
     rgb_name = "rgb_1_"+str(random_num)+".png"
     synth_name = "synthdepth_1_"+str(random_num)+".png"
     return depth_name,rgb_name,synth_name
@@ -27,17 +27,17 @@ def create_image_plots():
     depth_name,rgb_name,synth_name = get_random_images()
     imgD = cv.imread(depth_name, 0)
     imgR = cv.imread(rgb_name, 0)
-    imgS = cv.imread(synth_name, 0)
- 
+    imgS = cv.imread("synthdepth_3_0018190.png", 0)
+    
     stereo = cv.StereoBM_create(numDisparities = 16,blockSize = 15)
  
     disparity = stereo.compute(imgR, imgD)
  
     plt.imshow(imgD)
     plt.subplot(1, 3, 1)
-    plt.imshow(imgR, 'gray')
+    plt.imshow(imgR)
     plt.subplot(1, 3, 2)
-    plt.imshow(imgS, 'gray')
+    plt.imshow(imgS)
     plt.subplot(1, 3, 3)
     plt.show()   
 
---
+++ src/test.py
@@ -0,0 +1,15 @@
+import math
+import numpy as np
+import cv2 as cv
+import os
+import matplotlib.pyplot as plt
+import sys
+path = "/home/preston/Documents/dataset/train"
+os.chdir(path)
+#"synthdepth_1_"+str(random_num)+".png"
+imgD ="synthdepth_3_0018190.png"
+imgD = cv.imread(imgD, 0)
+
+plt.imshow(imgD)
+plt.show()
+
